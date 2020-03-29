e912e7adcd18c76b30185369ed01257f4d32c8df
ariel murphy <arielladders@gmail.com>
2020-03-29 19:39:30 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,48 @@
+class Artist 
+attr_reader :name, :import 
+
+@@all = [] 
+
+def self.all 
+@@all    
+end
+
+def self.find_or_create_by_name(import)
+
+found_artist = @@all.find{|a| a.name == import} 
+
+if found_artist 
+    found_artist
+else 
+    self.new(import)
+end 
+
+end
+
+
+def initialize(name)
+    @name = name 
+    @@all << self 
+    @songs = [] 
+end
+
+def name=(name)
+    @name = name 
+end
+
+def add_song(song) 
+@songs << song 
+song.artist = self 
+end
+
+def songs 
+@songs
+end
+
+def print_songs 
+    @songs.each do |a|
+        puts a.name 
+    end
+end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+class Song 
+    attr_accessor :name, :artist, :import
+    @@all = [] 
+
+    def self.new_by_filename(import)
+        title = import.split("-")[1].strip 
+        artist_from_file = import.split("-")[0].strip
+        song = self.new(title)
+        song.artist = Artist.find_or_create_by_name(artist_from_file) 
+        song.artist.add_song(song)
+        song
+        # binding.pry 
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end
+
+    def artist_name=(import)
+    self.artist = Artist.find_or_create_by_name(import)
+    end
+
+
+
+end
